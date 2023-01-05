7fad9135620d8f32493c77af2f1d86cdfd9dbeb0
Haru <haru@haru-studio.co.kr>
2023-01-05 19:13:04 +0900

    Feat: Add Loop sample code

--- App.js
+++ App.js
@@ -1,39 +1,10 @@
-import React, { PureComponent } from "react";
-import { AppRegistry, StyleSheet, StatusBar } from "react-native";
-import { GameEngine } from "react-native-game-engine";
-import { Finger } from "./renderers";
-import { MoveFinger } from "./systems"
+import React from 'react';
+import DefaultTest from './DefaultTest';
+import LoopTest from './LoopTest';
 
-class BestGameEver extends PureComponent {
-  constructor() {
-    super();
-  }
 
-  render() {
-    return (
-      <GameEngine
-        style={styles.container}
-        systems={[MoveFinger]}
-        entities={{
-          1: { position: [40,  200], renderer: <Finger />}, //-- Notice that each entity has a unique id (required)
-          2: { position: [100, 200], renderer: <Finger />}, //-- and a renderer property (optional). If no renderer
-          3: { position: [160, 200], renderer: <Finger />}, //-- is supplied with the entity - it won't get displayed.
-          4: { position: [220, 200], renderer: <Finger />},
-          5: { position: [280, 200], renderer: <Finger />}
-        }}>
-
-        <StatusBar hidden={true} />
-
-      </GameEngine>
-    );
-  }
+const App = () => {
+  return <LoopTest/>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFF"
-  }
-});
-
-export default BestGameEver;
+export default App;
